5ae74003db15ce5959e497c8c1dff8127edab15d
goldcove <goldcove@users.noreply.github.com>
2020-01-30 20:11:18 +0100

    Added note about motor wiring issues.

--- watchwinder.ino
+++ watchwinder.ino
@@ -21,6 +21,7 @@ int motorPin4=11;
 Stepper steppmotor(stepsPerRevolution, motorPin4, motorPin2, motorPin3, motorPin1); //initialize the stepper. In accordance with your motor. See https://www.arduino.cc/en/reference/stepper
 /*
  * NOTE: Non-standard pin order for the Welleman 401 stepper motor. See https://forum.vellemanprojects.eu/t/no-reverse-arduino-uno-velleman-vma401-5vdc-stepper-motor/14362/2
+ * Motor wires 1 and 4 are switched and different from the example in stepper.h background info: https://www.tigoe.com/pcomp/code/circuits/motors/stepper-motors/
  */
 
 int turndirection; //Motor turnning direction. 0=both, 1=clockwise, 2=counterclockwise. In accordance with your watch requirements.
